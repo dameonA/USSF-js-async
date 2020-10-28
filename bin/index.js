1d8a78b83812ed28e901a412d4eafa3b55bcbf49
D Adolphin <dameon@Ds-MacBook-Pro.hsd1.wa.comcast.net>
2020-10-28 08:44:15 -0700

    updating my index.js

--- bin/index.js
+++ bin/index.js
@@ -1,45 +1,53 @@
 #!/usr/bin/env node
 const fetch = require('node-fetch')
-const data = {username: 'example'};
+const fs = require ('fs')
+// const data = {username: 'example'};
+let poke_input = fs.readFileSync('bin/input.txt', 'utf8')
 
-
-//console.log( "Hello!" );
-
-var fs = require('fs');
-
-var fileData = fs.readFileSync('input.txt');
-var content = fileData.toString().split('\n')
-
-function fetchPokemonName(){
-    fetch('https://pokeapi.co/api/v2/pokemon/')
-    .then(response => response.json())
-    .then(function(allpokemon){
-        allpokemon.results.forEach(function(pokemon){
-            console.log(pokemon);
-        })
-    })
-
-}
-fetchPokemonName();
-async function postData( url = 'https://pokeapi.co/api/v2/pokemon/', data= {}) {
-    const response = await fetch(url, {
-        method: 'POST',
-        mode: 'cors', 
-        cache: 'no-cache',
-        credentials: 'same-origin',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data),
-    })
-    .then(response => response.json())
+poke_input = poke_input.replace(/(\r)/gm, '')
+let poke_input_arr = poke_input.split('\n')
+poke_input_arr = poke_input_arr.map(entry => entry.toLowerCase());
+async function read_pokemon_data(name){
+await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
+    .then(data => data.json())
     .then(data => {
-      console.log('Success:', data);
-    })
-    .catch((error) => {
-      console.error('Error:', error);
-    });
+        console.log(name[0].toUpperCase() + name.substring(1) + ':' + data.types.map(element => element.type.name).join(', '))
+         })
+         .catch(err => console.log(err))
+        }
+        poke_input_arr.forEach(entry => read_pokemon_data(entry))
+
+//file.readFileSync('/Users/dameon/USSF-js-async/bin/input.txt').toString().split('\n')
+//     .forEach(pokemon => {
+//     fetch('https://pokeapi.co/api/v2/pokemon/${pokemon}/')
+//     .then(response => response.json())
+//     .then(data => {
+//         console.log('${data.name}: ${data.types.map(classification => classification.type.name).join(', ')}')
+//         });
+ 
+//     });
+
+
+// fetchPokemonName();
+// async function postData( url = 'https://pokeapi.co/api/v2/pokemon/', data= {}) {
+//     const response = await fetch(url, {
+//         method: 'POST',
+//         mode: 'cors', 
+//         cache: 'no-cache',
+//         credentials: 'same-origin',
+//         headers: {
+//             'Content-Type': 'application/json'
+//         },
+//         body: JSON.stringify(data),
+//     })
+//     .then(response => response.json())
+//     .then(data => {
+//       console.log('Success:', data);
+//     })
+//     .catch((error) => {
+//       console.error('Error:', error);
+//     });
     
-    }
+//     }
 
     
